8132fa5551fc03f5c8816d75299347e84a4f37ab
Smijj <61775765+Smijj@users.noreply.github.com>
2020-08-28 13:22:00 +0930

    added a service worker

--- js/javascript.js
+++ js/javascript.js
@@ -11,3 +11,17 @@ function overlay() {
     el = document.getElementById("overlay");
     el.style.display = (el.style.display == "block") ? "none" : "block";
 }
+
+// Loading in a service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ',
+            registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
