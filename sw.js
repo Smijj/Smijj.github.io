47c573d731e707d0924caef5a7de3d9a8553ffe7
Smijj <61775765+Smijj@users.noreply.github.com>
2020-08-28 13:22:45 +0930

    added sw.js file

---
+++ sw.js
@@ -0,0 +1,33 @@
+var CACHE_TITLE = 'my-site-cache';
+var CACHE_VERSION = 'v1';
+var CACHE_NAME = CACHE_TITLE + '-' + CACHE_VERSION;
+var urlsToCache = [
+'/',
+'/css/style.css',
+'/js/javascript.js'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );  
+});
+
+// Adding offline functionality
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
